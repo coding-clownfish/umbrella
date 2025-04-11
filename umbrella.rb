eb42207f765d5fa17e8055320278d7067045c657
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-04-11 00:10:03 +0000

    commit

--- umbrella.rb
+++ umbrella.rb
@@ -1,1 +1,21 @@
 # Write your solution below!
+#source "https://rubygems.org"
+#gem "dotenv"
+
+require "http"
+require "dotenv/load"
+require "jasn"
+
+#ask the user for their location
+
+pp "Where do you live?"
+gets location_info
+
+#Get and store user location
+
+#Get user latitude and longitude from the Google Maps API
+
+#Get the weather at the user’s coordinates from the Pirate Weather AP
+
+#Display the current temperature and summary of the weather for the next hour
+
