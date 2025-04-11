e0e04c37d66c6059e12e2ac626354c9144ef936d
coding-clownfish <204938594+coding-clownfish@users.noreply.github.com>
2025-04-11 00:23:33 +0000

    user

--- umbrella.rb
+++ umbrella.rb
@@ -4,18 +4,23 @@
 
 require "http"
 require "dotenv/load"
-require "jasn"
+require "json"
+
+pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_key + "/"
 
 #ask the user for their location
 
-pp "Where do you live?"
-gets location_info
-
-#Get and store user location
+pp "Where are you located?"
+user_location= gets.chomp
+pp user_location
 
 #Get user latitude and longitude from the Google Maps API
+
+maps_url="https://maps.googleapis.com/maps/api/geocode/json?address="+ user_location + "&key="+ ENV.fetch("GMAPS_KEY")
+pp maps_url
+ 
 
 #Get the weather at the user’s coordinates from the Pirate Weather AP
 
 #Display the current temperature and summary of the weather for the next hour
-
